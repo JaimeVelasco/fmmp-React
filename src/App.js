f0b215b2b8c9c042752b2f7335671721a2c173c8
JaimeVelasco <contact@jaimevelas.co>
2017-02-20 16:02:01 -0600

    Working trough egghead tutorial

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
-import React from 'react';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import Main from './main';
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Swapi from './Swapi';
 
 
-class App extends React.Component {
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      txt: 'this is the state text'
+    }
+  }
+
+  update(e){
+    this.setState({txt: e.target.value})
+  }
+
   render() {
+    const dynamicInput = {
+      margin: '0.5em',
+      color: 'black'
+    };
+
     return (
-      <MuiThemeProvider>
-        <Main />
-      </MuiThemeProvider>
-    )
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+          <FormInput update={this.update.bind(this)}/>
+          <h1 style={dynamicInput}>{this.state.txt}</h1>
+        </div>
+        <Swapi />
+      </div>
+    );
   }
 }
 
-export default App
+
+const FormInput = (props) =>
+<input type="text" onChange={props.update} />
+
+export default App;
